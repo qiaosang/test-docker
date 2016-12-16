0b72e6e3c0923b0927fada01b3bc28e51c634111
wenjianzhou <nihao-wenjian@163.com>
2016-12-16 18:15:42 +0800

    test

--- centos/test.sh
+++ centos/test.sh
@@ -4,12 +4,8 @@
 
 TEST_NAME=centos
 
-docker build --no-cache -t ${TEST_NAME} .
+docker build -t ${TEST_NAME} .
 
 docker run -i --name ${TEST_NAME} ${TEST_NAME}
 
-docker rm -f ${TEST_NAME}
-
-docker rmi ${TEST_NAME}
-
-#docker logs -f ${TEST_NAME}
+docker logs -f ${TEST_NAME}
