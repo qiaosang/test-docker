6629b188988308b02d91da47e2b4b619bce1bf86
wenjianzhou <nihao-wenjian@163.com>
2018-04-24 15:19:24 +0800

    test

--- centos/test.cmd
+++
@@ -1,7 +0,0 @@
-set TEST_NAME=test
-
-docker build -t %TEST_NAME% .
-
-docker run -d --name %TEST_NAME% %TEST_NAME%
-
-docker logs -f %TEST_NAME%
--- centos/tset.cmd
+++
@@ -1,7 +0,0 @@
-set TEST_NAME=test
-
-docker stop %TEST_NAME%
-
-docker rm %TEST_NAME%
-
-docker rmi %TEST_NAME%
